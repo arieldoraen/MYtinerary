0ccd4f96475f490cc256f2768d17f14ff6f1c679
arieldoraen <ariel.doraen@gmail.com>
2019-11-01 10:09:11 -0300

    First upload

---
+++ my-app/src/models/Cities.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import '../App.css';
+
+
+function Cities() {
+  return (
+    <div className="App content" >
+      <Header />
+      <CityList />
+      <Footer />
+    </div>
+  );
+}
+
+class CityList extends React.Component {
+  render() {
+    return <div className="row">
+
+      cities
+        </div>
+
+
+  }
+}
+export default Cities;
---
+++ my-app/src/models/MYtinerary.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import '../App.css';
+
+
+function MYtinerary() {
+  return (
+    <div className="App content" >
+      <Header />
+      MYtinerary
+      <Footer />
+    </div>
+  );
+}
+
+export default MYtinerary;
