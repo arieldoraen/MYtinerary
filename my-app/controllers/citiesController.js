4c3ead29bccc57272e0b1da5f5be5cecf15c8e4b
Ariel Dos Santos <ariel.doraen@gmail.com>
2019-11-12 14:27:13 -0300

    List cities runing

--- my-app/controllers/citiesController.js
+++ my-app/controllers/citiesController.js
@@ -1,17 +1,20 @@
 const City = require('../models/citiesModel')
 
 const cityController = {
-    listCities: (req, res) => {
-        var Cities = City.find()
-        res.json({ "response": Cities })
+    listCities: async (req, res) => {
+        const data  = await City.find()
+        .catch(err => res.json({ 'response': err }))
+        res.json({ 'response': data })
     },
     loadCity: async(req, res) => {
         var name = req.body.name
         var country = req.body.country
+        var image  = req.body.image || ' '
 
         const newCity = new City({
             name: name,
-            country: country
+            country: country,
+            image: image
         })
 
         await newCity.save()
@@ -29,7 +32,8 @@ const cityController = {
                 id: req.body.id
             }, {
                 name: req.body.newName || req.body.name,
-                country: req.body.newCountry || req.body.country
+                country: req.body.newCountry || req.body.country,
+                image: req.body.newImage || req.body.image || ' '
             }, {
                 new: true
             })
--- my-app/models/citiesModel.js
+++ my-app/models/citiesModel.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const citySchema = new mongoose.Schema({
     name: {type: String, required: true},
-    country: {type: String, required: true}
+    country: {type: String, required: true},
+    image: {type: String, required: false}
 })
 
 const City = mongoose.model('cities', citySchema)
