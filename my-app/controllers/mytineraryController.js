35e75e0660518841a42b2996b6943c3c0b16a2fd
= <=>
2019-11-25 12:04:59 -0300

    schemas & models 2

--- my-app/controllers/mytineraryController.js
+++ my-app/controllers/mytineraryController.js
@@ -7,8 +7,26 @@ const mytineraryController = {
     ).catch(err => res.json({ response: err }));
     res.json({ response: data });
   },
-  loadMYtinerary: async (req, res) => {},
+  loadMYtinerary: async (req, res) => {
+    var title = req.body.title;
+    var user_id = req.body.user_id;
+    var city_id = req.body.city_id;
+    var duration = req.body.duration;
+    var price = req.body.price;
+    var hashtag = req.body.hashtag;
 
+    const newMytinerary = new Mytinerary({
+      title: title,
+      user_id: user_id,
+      city_id: city_id,
+      duration: duration,
+      price: price,
+      hashtag: hashtag
+    });
+
+    await newMytinerary.save().catch(err => res.json({ response: err }));
+    res.json({ response: "OK" });
+  },
   deleteMYtinerary: async (req, res) => {},
   updateMYtinerary: async (req, res) => {}
 };
